77ac90931f36187c848392d1d45531073634c4cd
tsipkens <tsipkens@gmail.com>
2021-02-13 15:09:06 -0500

    Overhaul linear_idx
    Fixes various bugs and code structure issues.

--- +kernel/linear_idx.m
+++ +kernel/linear_idx.m
@@ -6,47 +6,39 @@
 %  
 %  AUTHOR: Timothy Sipkens, 2020-08-10
 
-%{
-function K = linear_ind(n_r, ih)
+function K = linear_idx(n_r, my)
 
-j = 1:n_r;
+ii = 0:(n_r-1);
+ih = ii ./ sqrt(1 + my .^ 2);
 
-n_h = length(ih); % number of rays
+% get range of rj
+jjd = (0:(n_r-3))';  % r_{j-1}
+jj  = (1:(n_r-2))';  % r_j
+jju = (2:(n_r-1))';  % r_{j+1}
 
-K = zeros(n_h, n_r); % pre-allocate
+% functions for indefinite integral
+Kb = @(ih,i,j) log(abs(j + sqrt(j.^2 - ih.^2)));
+Kc = @(ih,i,j1,j2,j3) Kb(ih, i, j3 + eps);
+    % the + eps allows for finite value of kernel when r3 = x0
 
-for ii=1:n_h % loope through rays
-    K(ii,:) = 2 .* ii .* (2.*log(abs(j + sqrt(j.^2 - ih(ii).^2))) - ...
-        log(abs(j-1 + sqrt((j-1).^2 - ih(ii).^2))) - ...
-        log(abs(j+1 + sqrt((j+1).^2 - ih(ii).^2))));
-end
+% Compute kernel.
+% '+eps' avoids division by zero at ii = 0.
+K = real(2 .* (ih .^ 2) ./ (ii+eps) .* ( ... % real(.) removes values outside integral bounds
+    [ ...
+     zeros(1,length(ih)); ... % max allows for either m or u0 to be a scalar
+     Kc(ih,ii,jjd,jj,jj) - ...
+     Kc(ih,ii,jjd,jj,jjd); ... % integral over rise
+     Kc(ih,ii,jj(end),jju(end),jju(end)) - ...
+     Kc(ih,ii,jj(end),jju(end),jj(end)) ... % incline, last element
+    ] + [
+     -Kc(ih,ii,jj(1),jjd(1),jj(1)) - ...
+     -Kc(ih,ii,jj(1),jjd(1),jjd(1)); ... % decline, first element
+     -Kc(ih,ii,jju,jj,jju) - ...
+     -Kc(ih,ii,jju,jj,jj); ... % integral over decline
+     zeros(1,length(ih))
+    ]))';
 
-end
-%}
-
-%-{
-function K = linear_idx(n_r, ih)
-
-j = 0:(n_r-1);
-
-n_h = length(ih); % number of rays
-
-K = zeros(n_h, n_r); % pre-allocate
-
-
-K(1,:) = zeros(size(j));
-
-for ii=1:(n_h-2) % loope through rays
-    K(ii+1,:) = 2 .* ii .* (...
-        2.*log(abs(j + sqrt(j.^2 - ih(ii+1).^2))) ...
-        - log(abs(j+1 + sqrt((j+1).^2 - ih(ii+1).^2))) ...
-        - log(abs(j-1 + sqrt((j-1).^2 - ih(ii+1).^2))));
-end
-
-K(n_h,:) = 2 .* ii .* (...
-        2.*log(abs(j + sqrt(j.^2 - ih(n_h).^2))) ...
-        - log(abs(j+1 + sqrt((j+1).^2 - ih(n_h).^2))) ...
-        - log(abs(j-1 + sqrt((j-1).^2 - ih(n_h).^2))));
+K(abs(K)<1e3*eps) = 0; % remove numerical noise
+K = sparse(K); % convert to a sparse matrix
 
 end
-%}
