ea6dffc89530ed1bc970d8e0ed5f83c850d26ca0
tsipkens <tsipkens@gmail.com>
2021-02-22 18:34:41 -0500

    Reorg. linear_d in attempt at speed
    + Several of the quantities are now pre-computed before the central loop.
    + Added idx_b and idx_c to limit the rays considered in front and back parts of the integrand.

--- +kernel/linear_d.m
+++ +kernel/linear_d.m
@@ -81,9 +81,9 @@ else  % consider 2D case
     
     % functions for indefinite integral
     A = @(my,y0,ry,ryu,r3) y0 ./ (ryu - ry) .* ...
-        log(abs(r3 + sqrt(r3.^2 - y0.^2 ./ (1+my.^2)))); % main part of integrand
-    Ad = @(my,ryd,ry,r1,r2) -(r2 - r1) ./ (ry - ryd) ...
-        .* my; % second part of integrand
+        log(abs(r3 + sqrt(r3.^2 - y0.^2 ./ (1+my.^2))));  % main part of integrand
+    Ad = @(my,ryd,ry,r1,r2) my .* ...
+        (r1 - r2) ./ (ry - ryd);  % second part of integrand
 
 
     N_beams = max([length(my), length(y0), ...
@@ -94,151 +94,187 @@ else  % consider 2D case
 
     Kx = spalloc(N_beams, aso2.Nx * (aso2.Nr + 1), ...
         round(1e-5 * N_beams * aso2.Nx * (aso2.Nr + 1)));
-
-
+    
+    o = [x0 + eps; y0; 0.*x0];
+    v = [mx; my; ones(size(my))]; v = normc(v);
+    d = (xj' - o(1,:)') ./ (v' * [1 0 0]'); % distance of start of ASO
+    % c = [x0; 0.*x0] + bsxfun(@times, v, d'); % adjust ray position to start of ASO
+    
+    
+    % Find radial intersection with axial elements.
+    % This will be a strictly positive number.
+    % This could be a bound for either term of the integrand.
+    rx0 = sqrt(1 ./ (1+my.^2) .* ...
+        ((1+my.^2) ./ mx .* ...
+        (xj - x0) + my.*y0) .^ 2 + ...
+        y0.^2); % lower edge, vector over u0 and v0
+    rxu0 = sqrt(1 ./ (1+my.^2) .* ...
+        ((1+my.^2) ./ mx .* ...
+        (xju - x0) + my.*y0) .^ 2 + ...
+        y0.^2); % upper edge, vector over u0 and v0
+    
+    
+    
+    
+    % Find z intersection with axial elements.
+    % This is used to determine whether to involve front of back
+    % portion of integral.
+    zu0  = (xj - x0) ./ mx;
+    zuu0 = (xju - x0) ./ mx;
+    
+    
+    %== COMMON PROPERTIES ======================================%
+    % Flag where intersect occurs.
+    fx0  = zu0 < (-my .* y0 ...
+        ./ (1 + my.^2));  % flags if intersect is in first integrand
+    fxu0 = zuu0 < (-my .* y0 ...
+        ./ (1 + my.^2));  % flags if upper intersect is in first integrand
+    
+    % Flag for +ive/-ive slope.
+    f_mx0 = repmat(mx<0, [aso2.Nx, 1]);
+    
+    % FOR: first integrand.
+    r1_a0 = rx0; r1_a0(f_mx0) = rxu0(f_mx0);
+    r2_a0 = rxu0; r2_a0(f_mx0) = rx0(f_mx0);
+    f1_a0 = fx0; f1_a0(f_mx0) = fxu0(f_mx0);
+    f2_a0 = fxu0; f2_a0(f_mx0) = fx0(f_mx0);
+    
+    % FOR: second integrand.
+    % Note that fx* flags are reversed from first integrand.
+    r1_b0 = rxu0; r1_b0(f_mx0) = rx0(f_mx0);
+    r2_b0 = rx0; r2_b0(f_mx0) = rxu0(f_mx0);
+    f1_b0 = ~fxu0; f1_b0(f_mx0) = ~fx0(f_mx0);
+    f2_b0 = ~fx0; f2_b0(f_mx0) = ~fxu0(f_mx0);
+    
+    
+    
     disp(' Looping through axial slices:');
-    tools.textbar(0);
-    for ii=1:(length(aso2.xe)-1) % loop through and append axial slices
-
-        % Find z intersection with axial elements.
-        % This is used to determine whether to involve front of back
-        % portion of integral.
-        zu  = (xj(ii) - x0) ./ mx;
-        zuu = (xju(ii) - x0) ./ mx;
-
-        % flag if ray intersects elements at all
+    tools.textbar([0,aso2.Nx]);
+    Kcol = [];
+    for ii=1:aso2.Nx % loop through and append axial slices
+        
+        % Get z intersection with axial elements.
+        zu  = zu0(ii,:);
+        zuu = zuu0(ii,:);
+        
+        
+        % Flag if ray intersects elements at all.
         f0 = or(or(or(and(zu > -aso2.R, zu < aso2.R), ... % if zv is in the bounds
             and(zuu > -aso2.R, zuu < aso2.R)), ... % if zvu is in the bounds
             and(zuu > 0, zu < 0)), ...
             and(zuu < 0, zu > 0)); % if there is a change of sign
         idx_a = 1:length(f0);
         idx_a = idx_a(f0); % indices of rays that intersect element
-
-
-
-        % Find radial intersection with axial elements.
-        % This will be a strictly positive number.
-        % This could be a bound for either term of the integrand.
-        rx = sqrt(1 ./ (1+my(idx_a).^2) .* ...
-            ((1+my(idx_a).^2) ./ mx(idx_a) .* ...
-            (xj(ii) - x0(idx_a)) + my(idx_a).*y0(idx_a)) .^ 2 + ...
-            y0(idx_a).^2); % lower edge, vector over u0 and v0
-        rxu = sqrt(1 ./ (1+my(idx_a).^2) .* ...
-            ((1+my(idx_a).^2) ./ mx(idx_a) .* ...
-            (xju(ii) - x0(idx_a)) + my(idx_a).*y0(idx_a)) .^ 2 + ...
-            y0(idx_a).^2); % upper edge, vector over u0 and v0
+        
+        
+        % Get radial intersection with axial elements.
+        rx = rx0(ii, idx_a); % lower edge, vector over u0 and v0
+        rxu = rxu0(ii, idx_a); % upper edge, vector over u0 and v0
 
 
 
         %== FIRST INTEGRAND ======================================%
-        % flag where intersect occurs
-        fx  = zu(idx_a)  < (-my(idx_a) .* y0(idx_a) ...
-            ./ (1 + my(idx_a).^2)); % flags if intersect is in first integrand
-        fxu = zuu(idx_a) < (-my(idx_a) .* y0(idx_a) ...
-            ./ (1 + my(idx_a).^2)); % flags if upper intersect is in first integrand
-
-        % reverse if slope is negative (i.e. rvu is encountered first)
-        r1 = rx; r1(mx(idx_a)<0) = rxu(mx(idx_a)<0);
-        r2 = rxu; r2(mx(idx_a)<0) = rx(mx(idx_a)<0);
-        f1 = fx; f1(mx(idx_a)<0) = fxu(mx(idx_a)<0);
-        f2 = fxu; f2(mx(idx_a)<0) = fx(mx(idx_a)<0);
-
-        % modified element widths
+        % Get where intersect occurs.
+        fx  = fx0(ii, idx_a); % flags if intersect is in first integrand
+        fxu = fxu0(ii, idx_a); % flags if upper intersect is in first integrand
+        
+        idx_b = idx_a(or(fx,fxu));  % get subset of rays for this integrand
+        
+        r1 = r1_a0(ii, idx_b);
+        r2 = r2_a0(ii, idx_b);
+        f1 = f1_a0(ii, idx_b);
+        f2 = f2_a0(ii, idx_b);
+        
+        % Modified element widths.
         % (adjusted for intersections with axial elements)
-        rjd = rjd0 .* ones(size(rx)); % repeat for relevant rv elements
+        rjd = rjd0 .* ones([1,length(idx_b)]); % repeat for relevant rv elements
         rjd(:,f1) = min(r1(f1), rjd(:,f1)); % adjust for lower axial bound
         rjd(:,f2) = max(r2(f2), rjd(:,f2)); % adjust for upper axial bound
 
-        rj = rj0 .* ones(size(rx));
+        rj = rj0 .* ones([1,length(idx_b)]);
         rj(:,f1) = min(r1(f1), rj(:,f1));
         rj(:,f2) = max(r2(f2), rj(:,f2));
 
-        rju = rju0 .* ones(size(rx));
+        rju = rju0 .* ones([1,length(idx_b)]);
         rju(:,f1) = min(r1(f1), rju(:,f1));
         rju(:,f2) = max(r2(f2), rju(:,f2));
-
+        
         % evaluate lower kernel (up to midpoint in ASO)
-        K1 = (1 ./ (1+my(idx_a).^2) .* ( ...
-            or(fx,fxu) .* ([ ...
+        K1 = (1 ./ (1+my(idx_b).^2) .* ( ...
+            ([ ...
              zeros(1,size(rj,2)); ...
-             A(my(idx_a), y0(idx_a), rjd0, rj0, rj) - ...
-             A(my(idx_a), y0(idx_a), rjd0, rj0, rjd) + ... % integral over rise
-             Ad(my(idx_a), rjd0, rj0, rjd, rj); ... % added component of integrand
-             A(my(idx_a), y0(idx_a), rj0(end,:), rju0(end,:), rju(end,:)) - ...
-             A(my(idx_a), y0(idx_a), rj0(end,:), rju0(end,:), rj(end,:)) + ...
-             Ad(my(idx_a), rj0(end,:), rju0(end,:), rj(end,:), rju(end,:)) ... % incline, last element
+             A(my(idx_b), y0(idx_b), rjd0, rj0, rj) - ...
+             A(my(idx_b), y0(idx_b), rjd0, rj0, rjd) + ... % integral over rise
+             Ad(my(idx_b), rjd0, rj0, rjd, rj); ... % added component of integrand
+             A(my(idx_b), y0(idx_b), rj0(end,:), rju0(end,:), rju(end,:)) - ...
+             A(my(idx_b), y0(idx_b), rj0(end,:), rju0(end,:), rj(end,:)) + ...
+             Ad(my(idx_b), rj0(end,:), rju0(end,:), rj(end,:), rju(end,:)) ... % incline, last element
             ] + [
-             A(my(idx_a), y0(idx_a), rj0(1,:), rjd0(1,:), rj(1,:)) - ...
-             A(my(idx_a), y0(idx_a), rj0(1,:), rjd0(1,:), rjd(1,:)) - ...
-             Ad(my(idx_a), rj0(1,:), rjd0(1,:), rj(1,:), rjd(1,:)); ...
-             A(my(idx_a), y0(idx_a), rju0, rj0, rju) - ...
-             A(my(idx_a), y0(idx_a), rju0, rj0, rj) - ...
-             Ad(my(idx_a), rju0, rj0, rju, rj); ... % integral over decline
+             A(my(idx_b), y0(idx_b), rj0(1,:), rjd0(1,:), rj(1,:)) - ...
+             A(my(idx_b), y0(idx_b), rj0(1,:), rjd0(1,:), rjd(1,:)) - ...
+             Ad(my(idx_b), rj0(1,:), rjd0(1,:), rj(1,:), rjd(1,:)); ...
+             A(my(idx_b), y0(idx_b), rju0, rj0, rju) - ...
+             A(my(idx_b), y0(idx_b), rju0, rj0, rj) - ...
+             Ad(my(idx_b), rju0, rj0, rju, rj); ... % integral over decline
              zeros(1,size(rj,2))
             ])))';
 
 
-
+        
         %== SECOND INTEGRAND =====================================%
-        % flag where intersect occurs
-        fx  = ~fx; % flags if intersect is in first integrand
-        fxu = ~fxu; % flags if upper intersect is in first integrand
-
-        % reverse if slope is negative (i.e. rvu is encountered first)
-        r1 = rxu; r1(mx(idx_a)<0) = rx(mx(idx_a)<0);
-        r2 = rx; r2(mx(idx_a)<0) = rxu(mx(idx_a)<0);
-        f1 = fxu; f1(mx(idx_a)<0) = fx(mx(idx_a)<0);
-        f2 = fx; f2(mx(idx_a)<0) = fxu(mx(idx_a)<0);
-
-        % modified element width
+        idx_c = idx_a(or(~fx,~fxu));
+        
+        r1 = r1_b0(ii, idx_c);
+        r2 = r2_b0(ii, idx_c);
+        f1 = f1_b0(ii, idx_c);
+        f2 = f2_b0(ii, idx_c);
+        
+        % Modified element width.
         % (adjusted for intersections with axial elements)
-        rjd = rjd0 .* ones(size(rx)); % repeat for relevant rv elements
+        rjd = rjd0 .* ones([1,length(idx_c)]); % repeat for relevant rv elements
         rjd(:,f1) = min(r1(f1), rjd(:,f1)); % adjust for lower axial bound
         rjd(:,f2) = max(r2(f2), rjd(:,f2)); % adjust for upper axial bound
 
-        rj = rj0 .* ones(size(rx));
+        rj = rj0 .* ones([1,length(idx_c)]);
         rj(:,f1) = min(r1(f1), rj(:,f1));
         rj(:,f2) = max(r2(f2), rj(:,f2));
 
-        rju = rju0 .* ones(size(rx));
+        rju = rju0 .* ones([1,length(idx_c)]);
         rju(:,f1) = min(r1(f1), rju(:,f1));
         rju(:,f2) = max(r2(f2), rju(:,f2));
-
+        
         % evaluate second integrand (beyond midpoint in ASO)
-        K2 = (1 ./ (1+my(idx_a).^2) .* ( ...
-            or(fx,fxu) .* ([ ...
+        K2 = (1 ./ (1+my(idx_c).^2) .* ( ...
+            ([ ...
              zeros(1, size(rj,2)); ...
-             A(my(idx_a), y0(idx_a), rjd0, rj0, rj) - ...
-             A(my(idx_a), y0(idx_a), rjd0, rj0, rjd) - ... % integral over rise
-             Ad(my(idx_a), rjd0,rj0, rjd, rj); ... % added component of integrand
-             A(my(idx_a), y0(idx_a), rj0(end,:), rju0(end,:), rju(end,:)) - ...
-             A(my(idx_a), y0(idx_a), rj0(end,:), rju0(end,:), rj(end,:)) - ...
-             Ad(my(idx_a), rj0(end,:), rju0(end,:), rj(end,:), rju(end,:)) ... % incline, last element
+             A(my(idx_c), y0(idx_c), rjd0, rj0, rj) - ...
+             A(my(idx_c), y0(idx_c), rjd0, rj0, rjd) - ... % integral over rise
+             Ad(my(idx_c), rjd0,rj0, rjd, rj); ... % added component of integrand
+             A(my(idx_c), y0(idx_c), rj0(end,:), rju0(end,:), rju(end,:)) - ...
+             A(my(idx_c), y0(idx_c), rj0(end,:), rju0(end,:), rj(end,:)) - ...
+             Ad(my(idx_c), rj0(end,:), rju0(end,:), rj(end,:), rju(end,:)) ... % incline, last element
             ] + [
-             A(my(idx_a), y0(idx_a), rj0(1,:), rjd0(1,:), rj(1,:)) - ...
-             A(my(idx_a), y0(idx_a), rj0(1,:), rjd0(1,:), rjd(1,:)) + ...
-             Ad(my(idx_a), rj0(1,:), rjd0(1,:), rj(1,:), rjd(1,:)); ...
-             A(my(idx_a), y0(idx_a), rju0, rj0, rju) - ...
-             A(my(idx_a), y0(idx_a), rju0, rj0, rj) + ...
-             Ad(my(idx_a), rju0, rj0, rju, rj); ... % integral over decline
+             A(my(idx_c), y0(idx_c), rj0(1,:), rjd0(1,:), rj(1,:)) - ...
+             A(my(idx_c), y0(idx_c), rj0(1,:), rjd0(1,:), rjd(1,:)) + ...
+             Ad(my(idx_c), rj0(1,:), rjd0(1,:), rj(1,:), rjd(1,:)); ...
+             A(my(idx_c), y0(idx_c), rju0, rj0, rju) - ...
+             A(my(idx_c), y0(idx_c), rju0, rj0, rj) + ...
+             Ad(my(idx_c), rju0, rj0, rju, rj); ... % integral over decline
              zeros(1,size(rj,2))
             ])))';
-
-
-        K0 = K1 + K2;
+        
+        K0 = zeros(length(idx_a), aso2.Nr+1);
+        K0(idx_b - idx_a(1) + 1, :) = K1;
+        K0(idx_c - idx_a(1) + 1, :) = K0(idx_c - idx_a(1) + 1, :) + K2;
         K0(isnan(K0)) = 0; % remove NaN values that result when modified element width is zero
-        K0(abs(K0)<100*eps) = 0; % remove numerical noise
+        K0(abs(K0)<1e3*eps) = 0;  % supress numerical noise
         K0 = sparse(K0);
-
-        if any(any(K(idx_a, ((ii-1)*(aso2.Nr+1)+1):(ii*(aso2.Nr+1)))~=0))
-            disp('HELP');
-        end
+        
         K(idx_a, ((ii-1)*(aso2.Nr+1)+1):(ii*(aso2.Nr+1))) = K0;
-
-
+        
         %== Evaluate axial deflections ===========================%
         %-{
-        Kx0 = ([zeros(1, size(rj,2)); ...
+        Kx0 = ([zeros(1, length(idx_a)); ...
              (rx - rjd0) ./ (rj0 - rjd0) .* and(rjd0 < rx, rx < rj0) - ...
              (rxu - rjd0) ./ (rj0 - rjd0) .* and(rjd0 < rxu, rxu < rj0); ...
              (rx - rjd0(end,:)) ./ (rj0(end,:) - rjd0(end,:)) .* and(rjd0(end,:) < rx, rx < rj0(end,:)) - ...
@@ -247,15 +283,33 @@ else  % consider 2D case
              (rxu - rju0(1,:)) ./ (rj0(1,:) - rju0(1,:)) .* and(rju0(1,:) < rxu, rxu < rj0(1,:)); ...
              (rx - rju0) ./ (rj0 - rju0) .* and(rju0 < rx, rx < rj0) - ...
              (rxu - rju0) ./ (rj0 - rju0) .* and(rju0 < rxu, rxu < rj0); ...
-             zeros(1,size(rj,2)) ... % if crosses in decline
+             zeros(1, length(idx_a)) ... % if crosses in decline
             ])';
+        Kx0(abs(Kx0)<1e3*eps) = 0;  % supress numerical noise
         Kx(idx_a, ((ii-1)*(aso2.Nr+1)+1):(ii*(aso2.Nr+1))) = sparse(Kx0);
+        % + remove derivative below
+        %}
+        
+        %{
+        Kx0 = (1 ./ sqrt(1 + mx(idx_a).^2) .* ([zeros(1, length(idx_a)); ...
+             rx .* (rx ./ 2 - rjd0) ./ (rj0 - rjd0) .* and(rjd0 < rx, rx < rj0) - ...
+             rxu .* (rxu ./ 2 - rjd0) ./ (rj0 - rjd0) .* and(rjd0 < rxu, rxu < rj0); ...
+             rx .* (rx ./ 2 - rjd0(end,:)) ./ (rj0(end,:) - rjd0(end,:)) .* and(rjd0(end,:) < rx, rx < rj0(end,:)) - ...
+             rxu .* (rxu ./ 2 - rjd0(end,:)) ./ (rj0(end,:) - rjd0(end,:)) .* and(rjd0(end,:) < rxu, rxu < rj0(end,:)) % if crosses in rise
+            ] + [rx .* (rx ./ 2 - rju0(1,:)) ./ (rj0(1,:) - rju0(1,:)) .* and(rju0(1,:) < rx, rx < rj0(1,:)) - ...
+             rxu .* (rxu ./ 2 - rju0(1,:)) ./ (rj0(1,:) - rju0(1,:)) .* and(rju0(1,:) < rxu, rxu < rj0(1,:)); ...
+             rx .* (rx ./ 2 - rju0) ./ (rj0 - rju0) .* and(rju0 < rx, rx < rj0) - ...
+             rxu .* (rxu ./ 2 - rju0) ./ (rj0 - rju0) .* and(rju0 < rxu, rxu < rj0); ...
+             zeros(1, length(idx_a)) ... % if crosses in decline
+            ]))';
+        Kx(idx_a, ((ii-1)*(aso2.Nr+1)+1):(ii*(aso2.Nr+1))) = sparse(Kx0);
+        % + add derivative below
         %}
 
-        tools.textbar(ii/(length(aso2.xe)-1));
+        tools.textbar([ii, aso2.Nx]);
     end
 
-
+    % Kx = Kx * aso2.Dx;
     tools.textheader;
 end
 
