cb881d40b5d74b8cec25bb1a1a44be47864bbe72
tsipkens <tsipkens@gmail.com>
2021-02-24 01:38:53 -0500

    Sweeping bug fixes to main2_compare
    These corresponds to changes in the Camera class that broke the traditional method.
    + Added code to switch which side of the ASO is used in the conventional methods.
    + Tweak to Case 1 camera position (moved closer).
    + Added a Case 5 phantom, with narrower jet start and broader expansion.

--- main2_compare.m
+++ main2_compare.m
@@ -23,7 +23,7 @@ aso2 = Aso2(R, Nr, X, Nx);
 %== Case studies / phantoms ==============================================%
 [xe, re] = meshgrid(aso2.xe(1:(end-1)), aso2.re);
 
-pha_no = 4;  % default jet is Pha. No. 3, Gaussian sphere is 4
+pha_no = 5;  % default jet is Pha. No. 3, Gaussian sphere is 4
 switch pha_no
     case 1
         bet2 = normpdf(re, 0, 0.5 .* (6 .* xe + 4)./(6 .* X + 4)); % spreading Gaussian jet
@@ -34,6 +34,8 @@ switch pha_no
     case 4
         bet2 = mvnpdf([re(:), xe(:)], ...
             [0,2], [0.3^2,0; 0,0.3^2]); % sphere
+    case 5
+        bet2 = normpdf(re, 0, 0.15 .* (3 .* xe + 4)./(X + 4)); % spreading Gaussian jet 2
 end
 bet2 = bet2(:);
 %=========================================================================%
@@ -48,7 +50,7 @@ Nu = 352;  % second image dimension
 cam_no = 1;
 switch cam_no
     case 1
-        oc = [3.5,0.5,-1.9];   % camera origin
+        oc = [2,0.45,-1.4];   % camera origin
         f = 1.5e2;          % focal length [px]
     case 2
         oc = [2,0,-20];      % camera origin
@@ -121,9 +123,10 @@ axis image;
 set(gca,'YDir','normal');
 colorbar;
 
+%%
 C0 = 2e-4; % scaling constant (i.e., epsilon > delta)
 
-u_of0 = -Kl2 * bet2;
+u_of0 = Kl2 * bet2;
 u_of0 = reshape(u_of0, [Nv, Nu]);
 
 noise_lvl = 0.1 .* max(max(u_of0));
@@ -178,7 +181,7 @@ imagesc(reshape(pois0, size(u_of)));
 colormap(flipud(balanced));
 pois_max = max(abs(pois0(:)));
 caxis([-pois_max, pois_max]);
-axis image; 
+axis image;
 colorbar;
 title('Poisson eq. solution');
 %}
@@ -186,23 +189,44 @@ title('Poisson eq. solution');
 
 
 %-- Only consider data above r = 0 ---------------------------------------%
-idx_xp = round(cam.y0, 6) >= 0; % removes eps that could remain
-Nu_a = sum(idx_xp) ./ Nu; % number of x entries above zero
+f_top = 1
+if f_top
+    idx_xp = round(cam.y0, 6) >= 0; % removes eps that could remain
+    Nu_a = sum(idx_xp) ./ Nu; % number of x entries above zero
 
-xa = round(flipud(reshape(cam.y0(idx_xp), [Nu_a,Nu])), 7);
-ya = round(reshape(cam.x0(idx_xp), [Nu_a,Nu]), 7);
+    ya = round(reshape(cam.y0(idx_xp), [Nu_a,Nu]), 7);
+    xa = round(reshape(cam.x0(idx_xp), [Nu_a,Nu]), 7);
 
-pois_half = -pois0(idx_xp);
-pois_half = flipud(reshape(pois_half, [Nu_a,Nu]));
-pois_half = pois_half(:);
+    pois_half = -pois0(idx_xp);
+    pois_half = reshape(pois_half, [Nu_a,Nu]);
+    pois_half = pois_half(:);
 
-int_half = -int0(idx_xp);
-int_half = flipud(reshape(int_half, [Nu_a,Nu]));
-int_half = int_half(:);
+    int_half = -int0(idx_xp);
+    int_half = reshape(int_half, [Nu_a,Nu]);
+    int_half = int_half(:);
 
-u_half = -u_of(idx_xp);
-u_half = flipud(reshape(u_half, [Nu_a,Nu]));
-u_half2 = u_half(:);
+    u_half = u_of(idx_xp);
+    u_half = reshape(u_half, [Nu_a,Nu]);
+    u_half2 = u_half(:);
+else
+    idx_xp = round(cam.y0, 6) <= 0; % removes eps that could remain
+    Nu_a = sum(idx_xp) ./ Nu; % number of x entries above zero
+
+    ya = -flipud(round(reshape(cam.y0(idx_xp), [Nu_a,Nu]), 7));
+    xa = flipud(round(reshape(cam.x0(idx_xp), [Nu_a,Nu]), 7));
+    
+    pois_half = -pois0(idx_xp);
+    pois_half = flipud(reshape(pois_half, [Nu_a,Nu]));
+    pois_half = pois_half(:);
+
+    int_half = -int0(idx_xp);
+    int_half = flipud(reshape(int_half, [Nu_a,Nu]));
+    int_half = int_half(:);
+
+    u_half = -u_of(idx_xp);
+    u_half = flipud(reshape(u_half, [Nu_a,Nu]));
+    u_half2 = u_half(:);
+end
 %-------------------------------------------------------------------------%
 
 
@@ -210,7 +234,8 @@ u_half2 = u_half(:);
 %   Direct approach.
 K_2pt = kernel.two_pt([Nu_a,Nu]);
 n_2pt = K_2pt * u_half2;
-n_2pta = interp2(ya, xa, reshape(n_2pt, [Nu_a,Nu]), ...
+n_2pta = interp2(xa, ya, ...
+    reshape(n_2pt, [Nu_a,Nu]), ...
     aso2.xe2, aso2.re2);  % interpolate back to aso2 space
 
 
@@ -228,7 +253,8 @@ title('Two point');
 %   Direct approach.
 K_s13 = kernel.simps13([Nu_a,Nu]);
 n_s13 = K_s13 * u_half2;
-n_s13a = interp2(ya, xa, reshape(n_s13, [Nu_a,Nu]), ...
+n_s13a = interp2(xa, ya, ...
+    reshape(n_s13, [Nu_a,Nu]), ...
     aso2.xe2, aso2.re2);
 
 
@@ -246,7 +272,8 @@ title('Simpson 13');
 %   Indirect approach.
 K_3pt = kernel.three_pt([Nu_a,Nu]);
 n_3pt = K_3pt * pois_half;
-n_3pta = interp2(ya, xa, reshape(n_3pt, [Nu_a,Nu]), ...
+n_3pta = interp2(xa, ya, ...
+    reshape(n_3pt, [Nu_a,Nu]), ...
     aso2.xe2, aso2.re2);
 
 figure(24);
@@ -260,7 +287,8 @@ title('Three point, Poisson');
 
 K_3pti = kernel.three_pt([Nu_a,Nu]);
 n_3pti = K_3pti * int_half;
-n_3ptia = interp2(ya, xa, reshape(n_3pti, [Nu_a,Nu]), ...
+n_3ptia = interp2(xa, ya, ...
+    reshape(n_3pti, [Nu_a,Nu]), ...
     aso2.xe2, aso2.re2);
 
 figure(25);
@@ -279,10 +307,11 @@ disp('Onion peeling...');
 W = kernel.onion_peel(size(u_half));
 
 L_tk2_op = regularize.tikhonov_lpr(2, size(u_half,1), size(W,2));
-A_tk2_op = [W; 2e2.*L_tk2_op];
+A_tk2_op = [W; 1e2.*L_tk2_op];
 b_tk2_op = [pois_half; sparse(zeros(size(L_tk2_op,1), 1))];
 n_op = full(lsqlin(A_tk2_op, b_tk2_op));
-n_opa = interp2(ya, xa, reshape(n_op, [Nu_a,Nu]), ...
+n_opa = interp2(xa, ya, ...
+    reshape(n_op, [Nu_a,Nu]), ...
     aso2.xe2, aso2.re2);
 
 figure(26);
@@ -308,7 +337,7 @@ disp('NRAP-L-D...');
 
 L_tk2_nrap = regularize.tikhonov_lpr(2, aso2.Nr+1, size(Kl2,2));
 A_tk2_nrap = [Kl2; 2e2.*L_tk2_nrap];  % 2e2 is regularization parameter
-b_tk2_nrap = [-u_of(:); sparse(zeros(size(L_tk2_nrap,1), 1))];
+b_tk2_nrap = [u_of(:); sparse(zeros(size(L_tk2_nrap,1), 1))];
 n_nrap = full(lsqlin(A_tk2_nrap, b_tk2_nrap));
 
 figure(27);
