350a0e419c41b19c6f229bda60e41f6228f2b81f
tsipkens <tsipkens@gmail.com>
2021-09-27 23:59:09 -0400

    Bug fix to arapd
    Missing 1 ./ (1 + my.^2) factor.

--- +transform/arapd.m
+++ +transform/arapd.m
@@ -7,7 +7,7 @@ function K = arapd(my, y0, r)
 
 r(r<y0/sqrt(1+my^2)) = NaN; % blank out irrelevant radii for u0 and m
 
-K = 2 .* y0 ./ ...
+K = 2 .* y0 ./ (1 + my.^2) ./ ...
     sqrt(r.^2 - y0.^2 ./ (1 + my.^2));
     % forward transform
 
